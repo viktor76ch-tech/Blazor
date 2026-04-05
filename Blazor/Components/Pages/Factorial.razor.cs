df65b215868385a1efaaf0add655f8c0a2f81374
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-05 14:05:37 +0500

    Factorial split DONE

---
+++ Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Factorial
+    {
+        int n;
+        BigInteger factorial;
+        void Calculate()
+        {
+            factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+        }
+    }
+}
