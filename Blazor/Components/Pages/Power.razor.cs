d65529d06710947e44f8edf689c16389842e6a19
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-06 22:19:16 +0500

    Power DONE

---
+++ Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        double num;
+        double pow;
+        double result;
+
+        void Calculate()
+        {
+            result = Math.Pow(num, pow);
+        }
+        
+    }
+}
