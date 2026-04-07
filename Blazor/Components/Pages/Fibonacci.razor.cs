e5defb0d1348ac7209decf21a7eb34048669b92f
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-07 18:35:24 +0500

    fibonacci DONE

---
+++ Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        int n = 0;
+        BigInteger a = 0, b = 1;
+        string fibonacciResult = "";
+        void FibonacciSeries()
+        {
+            a = 0; 
+            b = 1;
+            fibonacciResult = "";
+            for (int i = 0; i < n; i++)
+            {
+                fibonacciResult = fibonacciResult + " " + a;
+                BigInteger next = a + b;
+                a = b;
+                b = next;
+            }
+        }
+    }
+}
