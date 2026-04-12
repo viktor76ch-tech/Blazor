151564997815e094cbec97f51ce0df9c1e47e0ec
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-12 14:46:36 +0500

    TODOList in progress

---
+++ TODOList/TODOitem.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODOList
+{
+    public class TODOitem
+    {
+        public string Title { get; set; }
+        public bool DONE { get; set; }
+    }
+}
