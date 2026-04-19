f7bf3c9605ea503eaeeb7bfdb1a38bae50625677
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-19 16:56:49 +0500

    Занятие 3

---
+++ Movies/Migrations/20260419103447_mssql.local_migration_859.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_859 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "URL",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "URL",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
---
+++ Movies/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Title { get; set; }
+        [RangeAttribute(typeof(DateOnly), "1888-10-14", "9999-12-31")]
+        public DateOnly ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public string? URL { get; set; }
+        public string? Poster { get; set; }
+    }
+}
