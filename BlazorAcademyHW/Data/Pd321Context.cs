8cab5938c2267330604e2a666b389c3dc46bef20
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-04-24 20:09:15 +0500

    исправления

--- BlazorAcademyHW/Data/Pd321Context.cs
+++ BlazorAcademyHW/Data/Pd321Context.cs
@@ -36,7 +36,7 @@ public partial class Pd321Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-L9UEMDR\\SQLEXPRESS;Database=PD_321;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-10NOOGH\\SQLEXPRESS;Database=PD_321;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- BlazorAcademyHW/Models/Student.cs
+++ BlazorAcademyHW/Models/Student.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAcademyHW.Models;
 
 public partial class Student
 {
     [Key]
+    [Column("stud_id")]
     public int StudId { get; set; }
 
+    [Column("last_name")]
     public string LastName { get; set; } = null!;
 
+    [Column("first_name")]
     public string FirstName { get; set; } = null!;
 
+    [Column("middle_name")]
     public string? MiddleName { get; set; }
 
+    [Column("birth_date")]
     public DateOnly BirthDate { get; set; }
 
+    [Column("email")]
     public string? Email { get; set; }
 
+    [Column("phone")]
     public string? Phone { get; set; }
 
+    [Column("photo")]
     public byte[]? Photo { get; set; }
 
+    [Column("group")]
     public int? Group { get; set; }
 
     public virtual ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
