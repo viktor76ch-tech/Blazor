90d1370b5e1d16bcd723a1d4d3646553edc58bd7
V <Viktor76ch@gmail.com>
2026-04-29 13:53:09 +0500

    Добавление классов GroupScheduleDays и TeachersDisciplines

---
+++ BlazorAcademyHW/Models/GroupScheduleDays.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademyHW.Models
+{
+    public class GroupScheduleDays
+    {
+        [Column("group_id")]
+        public int GroupId { get; set; }
+
+        [Column("day_of_week")]
+        public byte DayOfWeek { get; set; } // 1 = Mon, 2 = Tue, ...
+
+        [ForeignKey("GroupId")]
+        public Groups? Group { get; set; }
+    }
+}
---
+++ BlazorAcademyHW/Models/TeachersDisciplines.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademyHW.Models
+{
+    public class TeachersDisciplines
+    {
+        [Column("teacher_id")]
+        public short TeacherId { get; set; }
+
+        [Column("discipline_id")]
+        public short DisciplineId { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public Teachers? Teacher { get; set; }
+
+        [ForeignKey("DisciplineId")]
+        public Disciplines? Discipline { get; set; }
+    }
+}
